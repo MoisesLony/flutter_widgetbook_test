aa1adb269e7033b54e61e2e89f374e11e14912ae
Moises Lony <54813324+MoisesLony@users.noreply.github.com>
2025-08-15 11:18:45 -0600

    refactor: reorganizar estructura de carpetas y componentes

--- lib/widgetbook/use_case/atoms/button_use_case.dart
+++ lib/widgetbook/use_case/atoms/button_use_case.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:widgetbook/widgetbook.dart';
 import 'package:widgetbook_annotation/widgetbook_annotation.dart' as widgetbook;
-import '../../atom/button.dart'; // Asegúrate de que esta ruta sea correcta
+import '../../components/atom/button.dart'; // Asegúrate de que esta ruta sea correcta
 
 @widgetbook.UseCase(
   name: 'Botón Interactivo', 
